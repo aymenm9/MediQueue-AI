69f20008ad51856817ab6d45cfebcd7093e482b2
aymen merad <ay28mene@gmail.com>
2025-02-26 12:27:32 +0100

    sucurty api key

--- app/ai_util.py
+++ app/ai_util.py
@@ -1,8 +1,8 @@
 from google import genai
-
+from key import API_KEY
 
 def test_ai():
-    client = genai.Client(api_key="AIzaSyBXyKGc-t5fOgeUViXHNfcCYEKTpAOnB5k")
+    client = genai.Client(api_key=API_KEY)
     chat = client.chats.create(model="gemini-2.0-flash")
     response = chat.send_message_stream("I have 2 dogs in my house.")
     for chunk in response:
